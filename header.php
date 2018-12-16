0d5336dec1351e2428b78d12caa6d96525da4fb7
nicolas.manceau <you@example.com>
2018-12-16 21:18:23 +0100

    fichier sépraré pour connexion base de données 16/12/2018

--- header.php
+++ header.php
@@ -18,14 +18,7 @@
 </head>
 
 <?php
-// Variable de connexion à la base de donnée
-$host_name = "localhost";
-$database = "mydb";
-$user_name = "user";
-$password = "user";
-
-// Connection à la base de donnée
-$connect = mysqli_connect($host_name, $user_name, $password, $database);
+include('connexion_bd.php');
 ?>
 
 <body class = "bg-blue">

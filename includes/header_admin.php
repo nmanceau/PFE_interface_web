bb15d378ec6ac0cea4d6669ecc16ed4e65e92e35
nicolas manceau <nicolas.manceau@outlook.fr>
2019-01-28 19:33:54 +0100

    ajout meta pour responsivité iphone

--- includes/header_admin.php
+++ includes/header_admin.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
+  <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <title>Mise en place de sondes connectées</title>
 
